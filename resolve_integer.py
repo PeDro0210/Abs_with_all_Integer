0a616e63c27869cbd06b8d42bf928f8b8e6d2f2f
Pedro Avila <avi23089@uvg.edu.gt>
2023-03-03 13:41:56 -0600

    Error_fix

--- resolve_integer.py
+++ resolve_integer.py
@@ -3,34 +3,24 @@ import random as rd
 import numpy as np
 
 def your_program():
-    # Write your code here
-    randomize = rd.randrange(100,-100,-1)
-    randomize2 = rd.randrange(100,-100,-1)
+    n = float(input("Enter the number of integers you want to generate: "))
+    randomize = np.negative(rd.randrange(n,-n,-1))
+    randomize2 = np.negative(rd.randrange(n,-n,-1))
 
-    integer_list= np.negative([randomize,randomize2])
 
+    integer_list= np.negative((np.arange(randomize,randomize2,1)))
+    np.random.shuffle(integer_list)
+        
     maxi = np.max(integer_list)
     mini = np.min(integer_list)
 
     maxabs = np.abs(maxi)
     minabs = np.abs(mini)
 
-
+    print("The list of integers is: ", integer_list)
     print("The maximum value is: ", maxabs)
     print("The minimum value is: ", minabs)
-    
-    
-def program_comparation():
-    #write the other program here
-    randomize = rd.randint(-100, 100)
-    randomize2 = rd.randint(-100, 100)
-
-
-    integer_list = np.array([-randomize, -randomize2])
-
-    maxabs = np.max(np.abs(integer_list))
-    minabs = np.min(np.abs(integer_list))
-
-    print("The maximum absolute value is:", maxabs)
-    print("The minimum absolute value is:", minabs)
+        
+your_program()
+#def program_comparation():
     
