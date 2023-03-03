6de8c324584c49aab8a5f571a5e7aa992d95bb96
PeDro0210 <pedroruben0210@gmail.com>
2023-03-02 23:24:21 -0600

    second_comit

--- resolve_integer.py
+++ resolve_integer.py
@@ -1,36 +1,13 @@
 #change libraries
 import random as rd
 import numpy as np
+import hashlib
 
 def your_program():
-    # Write your code here
-    randomize = rd.randrange(100,-100,-1)
-    randomize2 = rd.randrange(100,-100,-1)
-
-    integer_list= np.negative([randomize,randomize2])
-
-    maxi = np.max(integer_list)
-    mini = np.min(integer_list)
-
-    maxabs = np.abs(maxi)
-    minabs = np.abs(mini)
-
-
-    print("The maximum value is: ", maxabs)
-    print("The minimum value is: ", minabs)
-    
+    #your program
+    print("Your program")
     
 def program_comparation():
-    #write the other program here
-    randomize = rd.randint(-100, 100)
-    randomize2 = rd.randint(-100, 100)
+    #program comparation
+    print("Program comparation")
 
-
-    integer_list = np.array([-randomize, -randomize2])
-
-    maxabs = np.max(np.abs(integer_list))
-    minabs = np.min(np.abs(integer_list))
-
-    print("The maximum absolute value is:", maxabs)
-    print("The minimum absolute value is:", minabs)
-    
